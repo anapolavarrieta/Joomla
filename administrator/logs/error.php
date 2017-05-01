8a5662511cd9ddeaa37c5ebac35e408100bf7151
Ana Paula Olavarrieta <anapolavarrieta@gmail.com>
2017-04-30 22:51:42 -0500

    PrimerCOmit

---
+++ administrator/logs/error.php
@@ -0,0 +1,9 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2017-04-23 14:43:54 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2017-04-23T14:43:53+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2017-05-01T03:12:48+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2017-05-01T03:12:58+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
